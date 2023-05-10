40e4cbf73861b3c6d46be3d1ff7dd75d433a2331
Mads Lundsby <mads@mekker.dk>
2023-05-10 20:54:14 +0200

    Fix path so it finally works

--- docs/modules/fetch/fetchGithubCommitActivity.mjs
+++ docs/modules/fetch/fetchGithubCommitActivity.mjs
@@ -1,5 +1,5 @@
 async function fetchGithubCommitActivity() {
-  const response = await fetch('../docs/data/githubCommitActivity.json')
+  const response = await fetch('./data/githubCommitActivity.json')
   const commitActivity = await response.json()
   return commitActivity;
 }
